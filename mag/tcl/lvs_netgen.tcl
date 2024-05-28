e0d32969fbe97f5a70e7391bbc53d2d787ebe145
kolos.koblasz <kolos.koblasz@gmailcom>
2024-05-28 17:33:35 +0200

    Top level LVS error

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -3,4 +3,6 @@ set source [readnet spice /dev/null]
 readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $source
 readnet verilog ../src/project.v $source
 readnet spice ../xschem/simulation/$project.spice $source
+#add an GL verilog of your digital blocks:
+readnet verilog ../verilog/gl/mixer_control.v
 lvs "$layout $project" "$source $project" $::env(PDK_ROOT)/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_KolosKoblasz_mixer (
     input  wire       VGND,
     input  wire       VPWR,
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -18,5 +18,32 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+    wire mc_gm_lo_n;
+    wire mc_gm_lo_p;
+
+    mixer_control mixer_control_inst(
+        .clk            (clk),
+        .n_rst          (n_rst),
+        .ext_lo_en      (ui_in[0]),
+        .ext_lo_n       (ui_in[1]),
+        .ext_lo_p       (ui_in[2]),
+        .int_lo_settings(ui_in[5:3]),
+        .lo_n           (mc_gm_lo_n),
+        .lo_p           (mc_gm_lo_p),
+        .VPWR           (VPWR),
+        .VGND           (VGND)
+    );
+
+    gilbert_mixer gilbert_mixer_inst(
+        .out_n  (ua[3]),
+        .out_p  (ua[2]),
+        .in_n   (ua[0]),
+        .in_p   (ua[1]),
+        .lo_n   (mc_gm_lo_n),
+        .lo_p   (mc_gm_lo_p),
+        .vdd    (VPWR),
+        .vss    (VGND)
+        );
 
 endmodule
